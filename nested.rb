5bd3788dfd404ba6fa85937c999924648031823b
grant smith <grantsmith1256@gmail.com>
2018-10-21 20:04:55 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -76,6 +76,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
+   end
+ end
     programmer_hash[:yukihiro_matsumoto]= {}
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
