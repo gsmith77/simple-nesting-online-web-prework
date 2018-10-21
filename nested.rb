71512990669f9ca50741bdfcd71ed8fe7757b848
grant smith <grantsmith1256@gmail.com>
2018-10-21 19:52:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -103,8 +103,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     known_for = programmer_hash[:alan_kay][:known_for]
-     known_for.delete
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
